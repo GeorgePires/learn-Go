cf7f91e84465b70115ab1055f151b8ef70748b32
GeorgePires <georgeasp.pi@gmail.com>
2023-02-23 23:11:56 -0300

    :test_tube: test/ add test for empty string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("George!")
-	want := "Hello, George!"
-
-	if got != want {
-		t.Errorf("got %s want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %q", got, want)
+		}
 	}
+	t.Run("saying hello", func(t *testing.T) {
+		got := Hello("George!")
+		want := "Hello, George!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'hello world' when an empty string is supplied ", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
